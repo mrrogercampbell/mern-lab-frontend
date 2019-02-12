9942a79533abba0ee9f45f98041bfbac9e16c6d6
Roger Campbell II <rc1336@gmail.com>
2019-02-12 12:08:06 -0500

    Now able to create new instances.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import {Route, Link} from 'react-router-dom'
 import Home from '../Home/Home'
+import Create from '../Create/Create'
 
 class App extends Component {
   render() {
@@ -11,13 +12,14 @@ class App extends Component {
           <Link to="/" className="nav-link">
             <h1>Home</h1>
           </Link>
-          <Link to="/" className="nav-link">
+          <Link to="/new" className="nav-link">
             <h1>Create</h1>
           </Link>
         </nav>
         <main>
           <h1>Welcome to the Fun With Animals App</h1>
           <Route path='/' exact render={(routerProps) => <Home {...routerProps} {...this.state}/>}/>
+          <Route path='/new' exact render={(routerProps) => <Create {...routerProps} {...this.state}/>}/>
         </main>
       </div>
     );
---
+++ src/components/Create/Create.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import './Create.css'
+// import {Route, Link} from 'react-router-dom'
+import axios from 'axios'
+
+class Create extends Component {
+    constructor(props){
+        super(props)
+            this.state = {
+                animalName: '',
+                photoUrl: '',
+                animalOrigin: '',
+                animalDetails: '',
+            }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(evt) {
+        const variable = evt.target.name
+        console.log(variable)
+        this.setState({[variable]: evt.target.value }, 
+        () => {
+            console.log(this.state)
+        })
+        // console.log(evt.target.name)
+        // console.log(evt.target.value)
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        axios.post('http://localhost:3001/', this.state)
+            .then(res => {
+                console.log(res)
+                console.log(res.data)
+            })
+        console.log("CreateNewAnimal Fired");
+    }
+   
+    render() {
+        return (
+            <div>
+                <h1>Hello From Create</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name of Animal:</label>
+                        <br/>
+                    <input type="text" name="animalName" value={this.state.value} onChange={this.handleChange} />
+                        <br/>
+                    <label>Photo URL:</label>
+                        <br/>
+                    <input type="url" name="photoUrl" value={this.state.value} onChange={this.handleChange} />
+                        <br/>
+                    <label>Animal Origin:</label>
+                        <br/>
+                    <input type="text" name="animalOrigin" value={this.state.value} onChange={this.handleChange} />
+                        <br/>
+                    <label>Details on this Particular:</label>
+                        <br/>
+                    <input type="text" name="animalDetails" value={this.state.value} onChange={this.handleChange} />
+                        <br/>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Create;
